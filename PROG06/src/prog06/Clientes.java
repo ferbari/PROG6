47bf54211835974de53d7c8bf6d94988398f7ed7
Desinweb <ferbari@gmail.com>
2020-05-04 11:33:05 +0200

    comienzo

---
+++ PROG06/src/prog06/Clientes.java
@@ -0,0 +1,31 @@
+/*
+Tarea para PROG06.
+Detalles de la tarea de esta unidad.
+Enunciado.
+Se trata de hacer una aplicación en Java que gestione los clientes de una empresa. Esos datos, se almacenarán en un fichero serializado, denominado clientes.dat.
+
+Los datos que se almacenarán sobre cada cliente son:
+
+NIF.
+Nombre.
+Teléfono.
+Dirección.
+Deuda.
+Mediante un menú se podrán realizar determinadas operaciones:
+
+
+package prog06;
+
+/**
+ *Datos clientes
+ * @author fernando
+ * @since 1.0
+ */
+public class Clientes {
+    
+   private String NIF;
+   private String nombre;
+   private int telefono;
+   private double deuda;
+    
+}
---
+++ PROG06/src/prog06/PROG06.java
@@ -0,0 +1,32 @@
+/*
+Añadir cliente. Esta opción pedirá los datos del cliente y añadirá el registro correspondiente en el fichero.
+Listar clientes. Recorrerá el fichero mostrando los clientes almacenados en el mismo.
+Buscar clientes. Pedirá al usuario el nif del cliente a buscar, y comprobará si existe en el fichero.
+Borrar cliente. Pedirá al usuario el nif del cliente a borrar, y si existe, lo borrará del fichero.
+Borrar fichero de clientes completamente. Elimina del disco el fichero clientes.dat
+Salir de la aplicación.
+Elabora el programa y un documento con un procesador de texto. El documento debe ser de tipo ".doc" (Microsoft Word) o de tipo ".odt" (OpenOffice.org). Debe tener tamaño de página A4, estilo de letra Times New Roman, tamaño 12 e interlineado normal.
+
+En el documento escribirás un informe sobre todas las consideraciones oportunas que se necesiten para entender cómo has realizado la tarea.
+ 
+ */
+package prog06;
+
+/**
+ *
+ * @author fernando
+ * @version 1.0
+ * @since 1
+ */
+public class PROG06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Menu");
+    }
+    
+}
